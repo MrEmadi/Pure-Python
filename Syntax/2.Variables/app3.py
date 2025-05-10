d7c3a216a83793e752776ae0f0119d2c92da14e6
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-05-11 00:35:47 +0330

    Add app3.py with rules and examples for variable naming conventions

---
+++ Syntax/2.Variables/app3.py
@@ -0,0 +1,26 @@
+# Variable naming conventions:
+# 1. English letters (uppercase and lowercase)
+# 2. Numbers (0-9) -> It must be not at the beginning of the name
+# 3. Underline (_)
+_var1 = True
+Var2 = 5
+# 2war = 32 -> wrong!
+readable = 'yes'
+# user name = 'Amir' -> wrong!
+# user-name = "Amir Emadi" # wrong! -> 'kebab case'
+# -------------------------------------
+# bad naming (It's not readable!):
+n = "Amirhossein"
+a = 25
+c = "Gorgan"
+
+str = "some text!" # bad naming -> reserved keyword in Python!
+
+# good naming (It's readable):
+username = "Amirhossein"
+age = 25
+city = "Gorgan"
+
+user_name = "Amirhossein Emadi" # good naming -> 'snake case'
+userName = "Zahra Emadi" # good naming -> 'camel case'
+UserName = "Bagher Emadi" # good naming -> 'pascal case'
