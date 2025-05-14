084960fb4871a763a2f57bc840369a39ac9e7eb5
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-05-14 10:15:56 +0330

    Add nested lists example demonstrating list indexing

---
+++ Syntax/17.Lists/app3.py
@@ -0,0 +1,10 @@
+# Nested Lists:
+iran = ['Tehran', 'Mashhad']
+usa = ['New York', 'California']
+cities = [iran, usa]
+print(cities)
+
+print(cities[0])
+
+print(cities[0][1])
+# -------------------------------------------
