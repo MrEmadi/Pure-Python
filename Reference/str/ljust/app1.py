107c95abd0c39a13c2d9fccb9fe32b68e6127a35
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-05-21 09:43:24 +0330

    docs: update info of ljust and rjust functions in the Ref folder

--- Reference/str/ljust/app1.py
+++ Reference/str/ljust/app1.py
@@ -1,1 +1,9 @@
-# todo
+# string.ljust(width, fill_char):
+# string -> 'str' data type
+# width -> int
+# fill_char = " " -> str
+# return type -> str
+
+print("Amirhossein".ljust(20))
+
+print("Amirhossein".ljust(20, '*'))
--- Reference/str/rjust/app1.py
+++ Reference/str/rjust/app1.py
@@ -1,1 +1,9 @@
-# todo
+# string.rjust(width, fill_char):
+# string -> 'str' data type
+# width -> int
+# fill_char = " " -> str
+# return type -> str
+
+print("Amirhossein".rjust(20))
+
+print("Amirhossein".rjust(20, '*'))
