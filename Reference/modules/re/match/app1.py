c01cdc709dc2dae2180fbb2836e027ce74fb31df
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-05-24 10:32:05 -0700

    docs: add match function from re module in the Ref folder

--- Reference/modules/re/match/app1.py
+++ Reference/modules/re/match/app1.py
@@ -1,1 +1,13 @@
-# todo
+# ptr.match(string, start, end):
+# ptr -> 'Pattern[str]' object
+# string -> str
+# start = 0 -> int
+# end = sys.maxsize -> int
+# return type -> object (Match[str]) or None
+# not found -> None
+
+import re
+
+print(re.compile('^\\d+$').match('Amir404'))
+
+print(re.compile('^\\d+$').match('404'))
