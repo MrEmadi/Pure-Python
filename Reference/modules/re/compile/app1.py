01dc68f994141821f4808d7d455e5943724d7f1d
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-05-24 10:09:01 -0700

    docs: add compile function from re module in the Ref folder

--- Reference/modules/re/compile/app1.py
+++ Reference/modules/re/compile/app1.py
@@ -1,1 +1,30 @@
-# todo
+# re.compile(ptr):
+# ptr -> str
+# return type -> object (Pattern[str])
+"""
+. -> a character
+\d -> numbers (0-9)
+\D -> not numbers (0-9)
+\w -> letters (a-z and A-Z)
+\W -> not letters (a-z and A-Z)
+\s -> whitespace
+\S -> not whitespace
+
+[] -> a set of characters
+[^] -> not a set of characters
+[0-9] -> all numbers
+[a-z] -> all lowercase letters
+^ -> beginning of the string
+$ -> end of the string
+
++ -> 1 or more characters
+* -> 0 or more characters
+? -> 0 or 1 character
+{n, m} -> length of characters
+"""
+
+import re
+
+print(re.compile('^\\d+$').match('Amir404'))
+
+print(re.compile('^\\d+$').match('404'))
