de300dfbfa14ab66a0dacd1628c329f7794e5906
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-05-27 20:53:46 +0330

    docs: add remove function (list folder) in the Ref folder

--- Reference/list/pop/app1.py
+++ Reference/list/pop/app1.py
@@ -2,6 +2,7 @@
 # list -> 'list' data type
 # index = -1 -> int
 # return type -> T (removed item: str, int or ...)
+# Empty list or index is out of range -> IndexError
 
 names = ['Zahra', 'Amir', 'Bagher']
 
---
+++ Reference/list/remove/app1.py
@@ -0,0 +1,11 @@
+# list.remove(value):
+# list -> 'list' data type
+# value -> T (str, int, ...)
+# return type -> None
+# not found -> ValueError
+
+names = ['Zahra', 'Amir', 'Bagher', 'Amir', 'Ali']
+print(names)
+
+names.remove('Amir')
+print(names)
